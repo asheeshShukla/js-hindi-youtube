34e8fb5c8b4edebd80b61f6ea2313362d95ecfcb
ASHEESH. <139006643+asheeshShukla@users.noreply.github.com>
2024-09-21 13:15:39 +0000

    Variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=12345//no changes constant varibles
+let accountEmail="asheesh@gmail.com"//let can be used for resolves Scope problems
+var accountPassward="123456"
+accountCity="Jaipur"//variables le skte hai but if not mention then no problem in code but good habbit as a programmer used variales
+let accountState;//if value not assign then it gives undefined as output
+//changes data dekhte hai change hota hai ya nhi?
+// accountId=2345  no changes constant varibles
+accountEmail="ashish222@gmail.com"
+accountPassward="23456"
+accountCity="Mumbai"
+console.table([accountEmail,accountPassward,accountCity,accountState])
+/*{
+  blocks are called scopes
+  Note. Prefer not use of Var keywoars because of issue in block and issue in functional scope  
+
+}
+*/
