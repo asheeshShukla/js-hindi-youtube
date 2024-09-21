a863810d758f8fc132da9d2cc6894ad3556cafb5
ASHEESH. <139006643+asheeshShukla@users.noreply.github.com>
2024-09-21 19:13:18 +0530

    Add files via upload

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";// treat all Js code as newer version
+//Alert statement
+/*alert(3+3)
+console.log(3+3)  not support because we use Node js not browser
+*/
+//data 
+//these all above premitive datatype
+// number->2 the power 53
+// BigInt
+// String -> " "
+// Boolean-> true false/false
+// null-> stanlone value
+// undefined
+// Symbol -> define uniqueness
+
+ //object
+ console.log(typeof "Asheesh")//string
+ console.log(typeof undefined)//give undefined
+ console.log(typeof null)//treat as object
+
+
