80c3f9f6d9b348161c7dbc38cf40ac16279ec103
ASHEESH. <139006643+asheeshShukla@users.noreply.github.com>
2024-09-23 13:04:19 +0530

    Add files via upload

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,76 @@
+// let score=33
+// console.log(typeof score) 
+// console.log((score))
+
+
+//let score="33"
+// console.log(typeof score) 
+//  console.log((score))
+ 
+
+//let score="33abc" 
+// console.log(typeof score) 
+//  console.log((score))
+
+ //how to conversion string in number
+ let score="333"
+ let valueInNumber=Number(score)
+ console.log(typeof valueInNumber)
+ console.log( valueInNumber)
+
+//how to convert number in string
+let somescore=333
+ let stringNumber=String(somescore)
+ console.log(typeof stringNumber)
+ console.log( stringNumber)
+
+ //boolean tpye conversion
+//  1-> true
+//  0-> false
+//  empty string -> false
+//  "hitesh" -> true
+ let IsloggedIn=1;
+ let booleanIsloggedIn= Boolean(IsloggedIn)
+ console.log(booleanIsloggedIn)
+
+
+ //*************************operation in javascript***************************
+//  let value=3;
+//  let negvalue=-value
+//  console.log(negvalue)// gives negative -3
+
+
+let str1="Hello"
+let str2=" Asheesh"
+let str3= str1+ str2
+console.log(str3)/// give Hello Asheesh
+
+
+console.log("1"+2)// is start in string then all value treat as a string 
+
+console.log(1+"2")// 12
+
+console.log(1+2+"2") //1+2=3 ,32
+
+console.log("1"+2+2) //give 122
+
+console.log(2+2)//4
+
+console.log(2-2)//0
+console.log(3%2)//1
+console.log(4/3)//1.33
+
+
+console.log(true) //true
+console.log(+true)//1
+
+let GameCounter=100;
+GameCounter++;
+console.log(GameCounter)//101
+
+
+
+
+
+
+
