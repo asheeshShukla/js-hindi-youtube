f791f36047349012f7ab397e09a95f1bd703704d
ASHEESH. <139006643+asheeshShukla@users.noreply.github.com>
2024-09-24 13:23:24 +0530

    Update datatype-summary.js

--- 01_basic/datatype-summary.js
+++ 01_basic/datatype-summary.js
@@ -30,6 +30,24 @@ myobject={
     name:"hitesh",
     age: 22,
 }
+//**************** stack and heap memory in java script***************
+//stack=> all primitive datatypes comes under stack
+
+//eg
+let myYoutube="asheeshshukla"
+let anotherName="myYoutube"
+anotherName="chai aur code"
+console.log(myYoutube)
+console.log(anotherName)
+//heap=>all primitive Non-datatypes comes under stack
+let user1={
+    Email: "asheeshshukla2k17@gmail.com",
+    UPI:"user@ybl"
+}
+let user2=user1
+user2.Email="asheeshshukla2k17@gmail.com"//return as a refrence not copy 
+console.log(user1.Email)
+console.log(user2.Email)
 console.log(myobject)
 
 
